65bef11d17287f577e0dd98e020f23567abf7946
Cristobal Cortez <theonlyjeff223@gmail.com>
2022-09-12 14:12:11 -0700

    Started implementation in serving data from our REST API

--- src/components/datashow.js
+++ src/components/datashow.js
@@ -1,7 +1,33 @@
+import React, { useEffect, useState } from "react";
+
+
+
 function DataShowcase(){
+    const URL = "http://localhost:8080/books/";
+    const [books, setBooks] = useState([]);
+    useEffect( () => {
+        fetch(URL)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            setBooks(data);
+        })
+        .catch((err) => {
+            console.log(err.message);
+        });
+    }, []);
     return(
         <div className="data-container">
-            <p> Data grid goes here</p>
+            {books.map( (book) => {
+                return(
+                    <div className="book-obj" key={book.id}>
+                        <h2 className="book-title">{book.book_name}</h2>
+                        <h2 className="book-type">{book.book_type}</h2>
+                        <h2 className="book-genre">{book.book_genre}</h2>
+                        <h2 className="book-comp">{book.book_completed}</h2>
+                    </div>
+                )
+            })}
         </div>
     )
 }
